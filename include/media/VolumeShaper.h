a8241f28c63a67b7278586780a7d7bc40c7f072b
argraur <argraur@gmail.com>
2018-08-08 07:36:35 +0000

    VolumeShaper: Parcelable should be initialized explicitly under Configuration
    Otherwise, this breaks build in many places, in audio, in hwcomposer
    
    Change-Id: Iba8f2874e98724d4d0c4c992754b96376eb7916a
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- include/media/VolumeShaper.h
+++ include/media/VolumeShaper.h
@@ -135,6 +135,7 @@ public:
         explicit Configuration(const Configuration &configuration)
             : Interpolator<S, T>(*static_cast<const Interpolator<S, T> *>(&configuration))
             , RefBase()
+            , Parcelable()
             , mType(configuration.mType)
             , mId(configuration.mId)
             , mOptionFlags(configuration.mOptionFlags)
